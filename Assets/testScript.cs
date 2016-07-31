11d57e21eebecb21903a84a608f1dd20436bc801
marshmelllo <robfarlow@gmail.com>
2016-07-30 23:29:10 -0400

    Change3
    Test script added

---
+++ Assets/testScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScript : MonoBehaviour {
+	//declare new rigidbody
+	Rigidbody rig;
+	// Use this for initialization
+	void Start () {
+		//set rigidbody to the attacted rigidbody component
+		rig = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if input for key space
+		if (Input.GetKey ("space")) {
+			//apply force of 10 units in y-axis (up)
+			rig.AddForce (new Vector3 (0, 10, 0));
+		}		
+	}
+}
