68c3bc8d81f7696e53c6c897e06d39e5b80f8c4b
marshmelllo <robfarlow@gmail.com>
2016-08-20 00:43:20 -0400

    Changes

--- Assets/Scripts/ResistanceCaluator.cs
+++ Assets/Scripts/ResistanceCaluator.cs
@@ -9,16 +9,12 @@ public class ResistanceCaluator : MonoBehaviour {
 	Material[] code = new Material[12];
 
 	void Start (){
-
 		//load all materials into an array in order
 		for(int k =0; k<12; k++){
-				code [k] = Resources.Load ("b"+k.ToString()) as Material;
-		
+			code [k] = Resources.Load ("Materials/bands/b"+k.ToString(),typeof(Material)) as Material;
 		}
 	}
-
 	//method to return which color a number is
-
 	Material getColor(int num){
 		return code [num];
 	}
@@ -29,7 +25,7 @@ public class ResistanceCaluator : MonoBehaviour {
 		//Create string from ohms input value
 		string sOhms = ohms.ToString();
 		//make sure it is atleast 3 digits(for 3 bands)
-		while (sOhms.Length < 3) {
+		while (sOhms.Length < 4) {
 			//add 0 (black) if it is not
 			sOhms = sOhms + "0";
 		}
@@ -39,8 +35,6 @@ public class ResistanceCaluator : MonoBehaviour {
 		}
 		return colors;
 }
-
-
 	public void changeColor(int ohms){
 		//change color
 		//load all bands into an array
@@ -51,12 +45,10 @@ public class ResistanceCaluator : MonoBehaviour {
 		}
 		//get colors and store in list
 		List<Material> colors = convertToColor(ohms);
-		foreach(GameObject band in bands){
-			band.GetComponent<Renderer> ().material.color = Color.red;
+		//for each band, set the color to the color based on the inputed resistance
+		foreach(GameObject band in bands){	
+			band.GetComponent<Renderer> ().material.color = colors [0].color;
 			colors.RemoveAt (0);
 		}
-
 	}
-
 }
-s
